3b48f6a1b574342a6de10d59c468712839c34016
marlonperillo <m3perillo@gmail.com>
2017-09-19 21:16:25 +0800

    name change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
 //Connect
-mongo.connect('mongodb://127.0.0.1/mongochat', function(err,db){
+mongo.connect('mongodb://127.0.0.1/appatdb', function(err,db){
 	if(err){
 		throw err;
 	}
